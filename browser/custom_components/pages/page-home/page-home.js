b288c0f7da06f33f4de34ed3ccef22a3cff730ab
Thomas <thomaslee3-c@my.cityu.edu.hk>
2015-11-05 21:38:01 +0000

    Draw a hard-coded route to the map by clicking the get direction button.

--- browser/custom_components/pages/page-home/page-home.js
+++ browser/custom_components/pages/page-home/page-home.js
@@ -14,7 +14,9 @@ App.Elements['page-home'] = Polymer({
     //behaviors: [],
 
     /* https://www.polymer-project.org/1.0/docs/devguide/events.html#event-listeners */
-    //listeners: {},
+    listeners: {
+        'getDirBtn.tap':'getDirBtnOnTap'
+    },
 
     /**
      * https://www.polymer-project.org/1.0/docs/devguide/properties.html
@@ -31,18 +33,27 @@ App.Elements['page-home'] = Polymer({
     properties: {
         path: {
             type: Array,
-            value: [
-                { latitude: 51.525831, longitude: -0.131919 },
-                { latitude: 51.526185, longitude: -0.132293 },
-                { latitude: 51.525870, longitude: -0.133213 },
-                { latitude: 51.523459, longitude: -0.130765 },
-                { latitude: 51.523325, longitude: -0.130961 },
-                { latitude: 51.523072, longitude: -0.131105 },
-                { latitude: 51.522869, longitude: -0.131550 },
-                { latitude: 51.523187, longitude: -0.131942 }
-            ]
+            value: []
         }
-    }
+
+    },
 
     /* Functions specific to this element go under here. */
+    getDirBtnOnTap: function(e){
+        //ajax....
+        var fromBackend = [
+            { latitude: 51.525831, longitude: -0.131919 },
+            { latitude: 51.526185, longitude: -0.132293 },
+            { latitude: 51.525870, longitude: -0.133213 },
+            { latitude: 51.523459, longitude: -0.130765 },
+            { latitude: 51.523325, longitude: -0.130961 },
+            { latitude: 51.523072, longitude: -0.131105 },
+            { latitude: 51.522869, longitude: -0.131550 },
+            { latitude: 51.523187, longitude: -0.131942 }
+        ];
+
+        this.set('path', fromBackend);
+
+    }
 });
+
