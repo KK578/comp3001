66c4f2bbf823bdcbe35be3b1d68d28ef7ed2fa9d
Aman Khosa <aman@XPS13.Home>
2015-11-10 04:22:09 +0000

    Added basic functionality, finds nearest park and alerts result, uses current center of map as location

--- browser/custom_components/pages/page-home/page-home.js
+++ browser/custom_components/pages/page-home/page-home.js
@@ -14,7 +14,10 @@ App.Elements['page-home'] = Polymer({
     //behaviors: [],
 
     /* https://www.polymer-project.org/1.0/docs/devguide/events.html#event-listeners */
-    //listeners: {},
+    listeners: {
+        'findParkBtn.tap': 'findParkBtnOnTap',
+        'google-map-search-results': 'foundResults'
+    },
 
     /**
      * https://www.polymer-project.org/1.0/docs/devguide/properties.html
@@ -28,7 +31,28 @@ App.Elements['page-home'] = Polymer({
      *  computed {string}
      *  observer {string}
      */
-    properties: {}
+    properties: {
+        /*results: {
+            type: Array,
+            value: []
+        }*/
+    },
 
     /* Functions specific to this element go under here. */
+
+    foundResults: function (e) {
+        var search = document.querySelector('google-map-search');
+        var p = search.results[0];
+        alert('The nearest park is: ' + p.name + ' (' + p.latitude + ',' + p.longitude + ')');
+    },
+
+    findParkBtnOnTap: function (e) {
+        var search = document.querySelector('google-map-search');
+        //search.longitude = parseFloat(51.5072);
+        //search.latitude = parseFloat(0.1275);
+        search.radius = parseFloat(1000);
+        search.query = 'park';
+        search.types = 'park';
+        search.search();
+    }
 });
