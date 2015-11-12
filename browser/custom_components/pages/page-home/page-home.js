7388abf5f3a277ba1f28f8ad0fa4f75fa80e39eb
Kevin Kwan <KK1241@googlemail.com>
2015-11-12 17:23:16 +0000

    Bind all maps to use same API key. Change alert to toast-message event.

--- browser/custom_components/pages/page-home/page-home.js
+++ browser/custom_components/pages/page-home/page-home.js
@@ -16,7 +16,7 @@ App.Elements['page-home'] = Polymer({
     /* https://www.polymer-project.org/1.0/docs/devguide/events.html#event-listeners */
     listeners: {
         'myLocationBtn.tap': 'myLocationBtnOnTap',
-	    'findParkBtn.tap': 'findParkBtnOnTap'
+        'findParkBtn.tap': 'findParkBtnOnTap'
         //'google-map-search-results': 'foundResults',
         //'api-load': 'onApiLoad'
     },
@@ -33,7 +33,13 @@ App.Elements['page-home'] = Polymer({
      *  computed {string}
      *  observer {string}
      */
-    properties: {},
+    properties: {
+        apiKey: {
+            type: String,
+            value: 'AIzaSyAWW2GYwT88DQhx09eAItjkdFnFNTBMckw',
+            readOnly: true
+        }
+    },
 
     /* Functions specific to this element go under here. */
     /*onApiLoad: function (e) {
@@ -49,7 +55,7 @@ App.Elements['page-home'] = Polymer({
 
     myLocationBtnOnTap: function (e) {
         var loc = document.querySelector('geo-location');
-        this.centerMap(loc.latitude,loc.longitude,15);
+        this.centerMap(loc.latitude, loc.longitude, 15);
     },
 
     /*foundResults: function (e) {
@@ -63,20 +69,23 @@ App.Elements['page-home'] = Polymer({
         var mapsAPI = document.querySelector('google-maps-api'); //might need to check if api is loaded first
 
         var loc = document.querySelector('geo-location');
-        var currentLocation = {lat: loc.latitude, lng: loc.longitude};
+        var currentLocation = { lat: loc.latitude, lng: loc.longitude };
 
         var self = this;
 
         function callback(results, status) {
-          if (status === mapsAPI.api.places.PlacesServiceStatus.OK) {
-            var p = results[0];
-            self.centerMap(p.geometry.location.lat(),p.geometry.location.lng(),15);
-            var parkMarker = document.getElementById('parkMarker');
-            parkMarker.latitude = p.geometry.location.lat();
-            parkMarker.longitude = p.geometry.location.lng();
-            parkMarker.animation = "BOUNCE";
-            alert('The nearest park is: ' + p.name);
-          }
+            if (status === mapsAPI.api.places.PlacesServiceStatus.OK) {
+                var p = results[0];
+                self.centerMap(p.geometry.location.lat(), p.geometry.location.lng(), 15);
+                var parkMarker = document.getElementById('parkMarker');
+                parkMarker.latitude = p.geometry.location.lat();
+                parkMarker.longitude = p.geometry.location.lng();
+                parkMarker.animation = "BOUNCE";
+                // Prefer to not use alert, so using a thingy set up which shows the message in a paper-toast. :D
+                self.fire('toast-message', {
+                    message: 'The nearest park is: "' + p.name + '".'
+                });
+            }
         }
 
         var service = new mapsAPI.api.places.PlacesService(document.createElement('div'));
