9275fb10201063a5cf3dba45f2bc48011b69a655
Kevin Kwan <KK1241@googlemail.com>
2015-11-20 16:52:12 +0000

    Add ajax requesting to backend team, temporarily using a hard coded route.

--- browser/custom_components/pages/page-home/page-home.js
+++ browser/custom_components/pages/page-home/page-home.js
@@ -15,7 +15,9 @@ App.Elements['page-home'] = Polymer({
 
     /* https://www.polymer-project.org/1.0/docs/devguide/events.html#event-listeners */
     listeners: {
-        'getDirBtn.tap':'getDirBtnOnTap'
+        'getDirBtn.tap':'getDirBtnOnTap',
+        'ajax.response': 'ajaxResponse',
+        'ajax.error': 'ajaxError'
     },
 
     /**
@@ -39,21 +41,45 @@ App.Elements['page-home'] = Polymer({
     },
 
     /* Functions specific to this element go under here. */
-    getDirBtnOnTap: function(e){
-        //ajax....
-        var fromBackend = [
-            { latitude: 51.525831, longitude: -0.131919 },
-            { latitude: 51.526185, longitude: -0.132293 },
-            { latitude: 51.525870, longitude: -0.133213 },
-            { latitude: 51.523459, longitude: -0.130765 },
-            { latitude: 51.523325, longitude: -0.130961 },
-            { latitude: 51.523072, longitude: -0.131105 },
-            { latitude: 51.522869, longitude: -0.131550 },
-            { latitude: 51.523187, longitude: -0.131942 }
-        ];
+    getDirBtnOnTap: function (e) {
+        var ajax = this.$.ajax;
+
+        ajax.params = {
+            start: 'n10lz',
+            destination: 'n87ng'
+        };
+
+        console.log(ajax.requestUrl);
+        ajax.generateRequest();
+    },
+    ajaxResponse: function (e) {
+        var detail = e.detail;
+        console.log(detail);
+
+        // DEBUG: Until backend is finalised.
+        var encodedPolyline = 'whqyH|{TJf@LRNLdB`@JJ@fDA?A?A?CBEHADg@?_AAoCMyNi@aBEMD?AAAC?C@EGA@CACC?AeBv@g@TeBfAwFlDoAz@e@ZQRi@j@sB`C}@pA[n@}@dDEV?N_B?eCEcBAg@?\nCICW@_AAoEAkCBeFCaDCsBAuDOc@Ie@SOe@YoA@g@o@}AsAyC_AwDcCeKyBbB}BfBg@f@eB`B}AfB{@~@aFpFwI|ImAhAmBvByCzCoEpE}DpD{DhDkBbB]^g@d@c@Tc@J}@Dq@B_BPwA\\m@V_@TiAl@oAz@]\\y@nA{@dBcArBmAhAy@ZWFgAPU@YEcAa@i@UgBu@iDsBeA{@{CyC}DeEaAUq@GgBMIHi@FWA[HoA`@UYy@a@gDMSEuBGS@oDM}Ca@s@Uq@Yw@k@wBkAaAaAs@]q@QeAOg@GU@?@?@ABADEBA@KlBBhBJpAaB\\w@Ao@Gs@Yw@e@oCs@G`@GtBEpE';
+
+        var mapAPI = this.$['map-canvas'].$.api.api;
+
+        var fromBackend = mapAPI.geometry.encoding.decodePath(encodedPolyline);
+
+        for (var i = 0; i < fromBackend.length; i++) {
+            fromBackend[i].lat = fromBackend[i].lat();
+            fromBackend[i].lng = fromBackend[i].lng();
+        }
 
         this.set('path', fromBackend);
+    },
+    ajaxError: function (e) {
+        var detail = e.detail;
+        console.log(detail);
 
+        this.fire('toast-message', {
+            message: 'Sorry, an error occurred while requesting a route.'
+        });
+
+        // DEBUG: Until Backend is finalised.
+        this.ajaxResponse(e);
     }
 });
 
