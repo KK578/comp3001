957c8a98044caf8ee41b6320dd6d5d28055bda81
Kevin Kwan <KK1241@googlemail.com>
2015-12-04 21:11:16 +0000

    Add test for interface with routing element.

--- browser/test/no2pollution-search/no2pollution-search-test.js
+++ browser/test/no2pollution-search/no2pollution-search-test.js
@@ -2,6 +2,7 @@ chai.should();
 
 describe('<no2pollution-search>', function () {
     var element;
+    var marker;
 
     before(function (done) {
         element = document.querySelector('no2pollution-search');
@@ -21,7 +22,7 @@ describe('<no2pollution-search>', function () {
         searchDialog.opened.should.equal(true);
     });
 
-    it('should show markers on submitting', function (done) {
+    it('should show markers on submitting search dialog', function (done) {
         var input = element.querySelector('#search-input');
         input.value = 'University College London, Gower Street, London, United Kingdom';
 
@@ -40,6 +41,7 @@ describe('<no2pollution-search>', function () {
                     var epsilon = 0.001;
                     if (Math.abs(markers[i].latitude - 51.5245592) < epsilon &&
                         Math.abs(markers[i].longitude - (-0.1340401)) < epsilon) {
+                        marker = markers[i];
                         window.clearInterval(handle);
                         done();
                     }
@@ -48,7 +50,21 @@ describe('<no2pollution-search>', function () {
         }, 100);
     });
 
-    it('should clear dialog box on next open', function () {
+    it('should fire "no2pollution-route" on clicking "Route Me" button', function (done) {
+        function listener(e) {
+            e.detail.destination.should.equal('WC1E6BT');
+
+            window.removeEventListener('no2pollution-route', listener);
+            done();
+        }
+
+        window.addEventListener('no2pollution-route', listener);
+
+        var button = marker.querySelector('paper-button');
+        button.click();
+    });
+
+    it('should clear dialog box on next search dialog open', function () {
         var input = element.querySelector('#search-input');
         input.value = 'University College London, Gower Street, London, United Kingdom';
 
