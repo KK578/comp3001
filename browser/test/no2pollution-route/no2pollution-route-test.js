a3361461c8497a00deb5a82df39f28a780b16729
Kevin Kwan <KK1241@googlemail.com>
2015-12-04 23:33:22 +0000

    Add simple ajax calling test.

--- browser/test/no2pollution-route/no2pollution-route-test.js
+++ browser/test/no2pollution-route/no2pollution-route-test.js
@@ -7,5 +7,14 @@ describe('<no2pollution-route>', function () {
 		element = document.querySelector('no2pollution-route');
 	});
 
-    it('has no tests written yet');
+    it('should make an ajax request on calling "sendRequest"', function () {
+        var ajax = element.querySelector('#ajax');
+        sinon.stub(ajax, 'generateRequest');
+
+        element.sendRequest('N10LZ', 'N87NG');
+
+        ajax.generateRequest.should.have.been.calledWith();
+        ajax.params.should.have.property('start').which.equal('N10LZ');
+        ajax.params.should.have.property('end').which.equal('N87NG');
+    });
 });
