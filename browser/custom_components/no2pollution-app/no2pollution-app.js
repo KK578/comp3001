7c6e9079ea88098c6a84f932f2263c3e51532840
Kevin Kwan <KK1241@googlemail.com>
2015-12-04 16:59:51 +0000

    Readjust to use live backend data.

--- browser/custom_components/no2pollution-app/no2pollution-app.js
+++ browser/custom_components/no2pollution-app/no2pollution-app.js
@@ -1,6 +1,6 @@
 App.Elements['no2pollution-app'] = Polymer({
     is: 'no2pollution-app',
-    
+
     /* Lifecycle Callbacks: https://www.polymer-project.org/1.0/docs/devguide/registering-elements.html
      * Note that the order that all elements are ready may not be reliable.
      * If order is important, access sibling elements within the attached method,
@@ -8,13 +8,11 @@ App.Elements['no2pollution-app'] = Polymer({
      */
     //created: function () {},
     //ready: function () {},
-    attached: function () {
-        this.async(this.ajaxResponse, 2000);
-    },
-    
+    //attached: function () {},
+
     /* https://www.polymer-project.org/1.0/docs/devguide/behaviors.html */
     //behaviors: [],
-    
+
     /* https://www.polymer-project.org/1.0/docs/devguide/events.html#event-listeners */
     listeners: {
         'myLocationBtn.tap': 'myLocationBtnOnTap',
@@ -25,7 +23,7 @@ App.Elements['no2pollution-app'] = Polymer({
         'park-found': 'foundPark',
         'template-paths.dom-change': 'setupInfo'
     },
-    
+
     /**
      * https://www.polymer-project.org/1.0/docs/devguide/properties.html
      *
@@ -48,7 +46,7 @@ App.Elements['no2pollution-app'] = Polymer({
         srchInput: "",
         results: ""
     },
-    
+
     /* Functions specific to this element go under here. */
     centerMap: function (lat, lng, zoom) {
         var map = document.querySelector('google-map');
@@ -56,32 +54,32 @@ App.Elements['no2pollution-app'] = Polymer({
         map.longitude = lng;
         map.zoom = zoom;
     },
-    
+
     setAccuracyCircle: function () {
         var myLocationMarker = document.getElementById('myLocationMarker');
         var loc = document.querySelector('geo-location');
         var radius = loc.position.coords.accuracy;
-        
+
         console.log("Location Accuracy: " + radius);
-        
+
         var circle = document.querySelector('map-circle');
         circle.setCircle(myLocationMarker.marker, radius);
     },
-    
+
     myLocationBtnOnTap: function (e) {
         var loc = document.querySelector('geo-location');
         this.centerMap(loc.latitude, loc.longitude, 15);
         this.setAccuracyCircle();
     },
-    
+
     findParkBtnOnTap: function (e) {
         var loc = document.querySelector('geo-location');
         var currentLocation = { lat: loc.latitude, lng: loc.longitude };
-        
+
         var parkFinder = document.querySelector('park-finder');
         parkFinder.findPark(currentLocation);
     },
-    
+
     foundPark: function (e) {
         var p = e.detail;
         this.centerMap(p.geometry.location.lat(), p.geometry.location.lng(), 15);
@@ -89,26 +87,26 @@ App.Elements['no2pollution-app'] = Polymer({
         parkMarker.latitude = p.geometry.location.lat();
         parkMarker.longitude = p.geometry.location.lng();
         parkMarker.animation = "BOUNCE";
-        
+
         this.fire('toast-message', {
             message: 'The nearest park is: "' + p.name + '".'
         });
     },
-    
+
     getDirBtnOnTap: function (e) {
         var srch_dialog = document.getElementById("search-dialog");
         if (srch_dialog) {
             srch_dialog.open();
         }
     },
-    
+
     srchReqOnEnter: function (e) {
         //check if 'enter' was pressed
         if (e.keyCode === 13) {
             // Empty results array before continuing to ensure all google-map-markers
             // are correctly detached before
             this.results = [];
-            
+
             this.set('criteria', this.srchInput);
             this.set('srchInput', '');
             var srch_dialog = document.getElementById("search-dialog");
@@ -117,23 +115,23 @@ App.Elements['no2pollution-app'] = Polymer({
             }
         }
     },
-    
+
     routeBtnOnTap: function (sender) {
         var location = JSON.parse(sender.getAttribute('location'));
         var postcodeRegex = /([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)/;
         // Backend currently uses postcode location to do routing.
         var postcode = location.formatted_address.match(postcodeRegex)[0];
-        
+
         var mapsAPI = document.querySelector('google-maps-api');
         var geocoder = new mapsAPI.api.Geocoder();
-        
+
         var currentLocation = {
             location: {
                 lat: this.userLatitude,
                 lng: this.userLongitude
             }
         };
-        
+
         geocoder.geocode(currentLocation, function (results) {
             var currentPostcode = '';
             for (var i = 0; i < results.length; i++) {
@@ -143,7 +141,7 @@ App.Elements['no2pollution-app'] = Polymer({
                     break;
                 }
             }
-            
+
             postcode = postcode.replace(' ', '');
             currentPostcode = currentPostcode.replace(' ', '');
             this.sendRequest(currentPostcode, postcode);
@@ -152,7 +150,7 @@ App.Elements['no2pollution-app'] = Polymer({
             });
         }.bind(this));
     },
-    
+
     on_api_load: function () {
         var mapsAPI = document.querySelector('google-maps-api');
         var map = document.querySelector("google-map");
@@ -163,7 +161,7 @@ App.Elements['no2pollution-app'] = Polymer({
             map: map.map,
             anchorPoint: new mapsAPI.api.Point(0, -29)
         });
-        
+
         autocomplete.addListener('place_changed', function () {
             this.set('results', '');
             infowindow.close();
@@ -173,7 +171,7 @@ App.Elements['no2pollution-app'] = Polymer({
                 console.log(place);
                 return;
             }
-            
+
             // If the place has a geometry, then present it on a map.
             if (place.geometry.viewport) {
                 map.map.fitBounds(place.geometry.viewport);
@@ -184,44 +182,39 @@ App.Elements['no2pollution-app'] = Polymer({
 
         });
     },
-    
+
     sendRequest: function (start, destination) {
         var ajax = this.$.ajax;
-        
+
         ajax.params = {
             start: start,
             end: destination
         };
-        
+
         ajax.generateRequest();
     },
     ajaxResponse: function (e) {
-        //var detail = e.detail.response;
-        // CrossOrigin issues again. :'D
-        var detail = [{ "start": "n10lz", "end": "n87ng", "polyline": "whqyH|{TJf@LRNLdB`@JJ@fDA?A?A?CBEHADg@?_AAoCMyNi@aBEMD?AAAC?C@EGA@CACC?AeBv@g@TeBfAwFlDoAz@e@ZQRi@j@sB`C}@pA[n@y@rCG\\AZ_DCeAAgBAc@?KCICW@a@?cBAiCAkCBeFCaDC_DE{COw@YWy@Q{@@g@uB}EQc@s@wCe@sBeBgHcAv@mA~@iCtBiBfBqC`DkExEgGjGiCjCq@l@mBvBqBpB_FdFuBhBeBdB{EdEsAlAw@v@c@Tc@JaBHU?wAPcAVaA\\_@TiAl@oAz@]\\y@nA{@dBcArBmAhAy@ZWFgAPU@YEcAa@i@UgBu@}BsAqA}@{DwDkCqCq@s@_ASCAiAMmAGIHi@FWAkBj@UYg@Yg@MqCGSE{BG}BG_ACoAQmAOUKgA]y@o@cBy@YSaAaAs@]q@QcBWWBUA?BABADC@A@A?ArCDdALpAaB\\w@Ao@Gs@Yw@e@oCs@G`@GtBEpE", "distance": 6717.760143754025, "maxNO2": 69, "avgNO2": 49, "maxO3": 31, "avgO3": 28, "maxPM10": 20, "avgPM10": 15, "maxPM25": 6, "avgPM25": 7 }, { "start": "n10lz", "end": "n87ng", "polyline": "whqyH|{TC[Be@Pk@HIJGtBSAsDBWEO?QCYGMSIIEEEa@KqBs@UQq@[g@MQBGqAUsGGUMKEMi@cFc@EW@gATwBf@}FxAeBj@iA`@_A`@aD`BaH|DoBjAwCjB}AnAwClBe@Z]{@yB}EQWKNIP]k@kBaCaA}AQg@OaA]}CYiCU_A]}@]m@q@q@WQUGa@CmAPgKtBCJGJG@IGCK_EbA_GxAEpAEVODuCT{BIMESUi@]OEU@cAa@_AUsBS{@CyAOCAKe@UHUHOFQ@oDQsAMOKSUMMI?IDO]aBY_ALS@GCOOwCfFgA`BiCxCyA`CkBjDmCpFmAjD_IbUKn@GlAUjCOr@s@hB[~@}@fC_BrDcBjDMZ]d@gClBo@`@aBz@u@\\cBt@aD`BaDdBiAv@_ExBe@VIJ]b@_ASCAwCUIHi@FWA[HoA`@UYg@Yg@MqCGSE{BG}BG_ACoAQmAOUKgA]y@o@cBy@YSaAaAs@]q@QcBWWBUA?@A@AFABE@ArCDdALpAaB\\w@Ao@Gs@Yw@e@oCs@G`@GtBEpE", "distance": 7207.600690712004, "maxNO2": 65, "avgNO2": 42, "maxO3": 27, "avgO3": 24, "maxPM10": 17, "avgPM10": 13, "maxPM25": 7, "avgPM25": 7 }, { "start": "n10lz", "end": "n87ng", "polyline": "whqyH|{TC[Be@Pk@HIJGtBSAsDBWEO?QCYGMSIIEEEa@KqBs@UQq@[g@MQBGqAUsGGUMKEMi@cFKyAOkDa@oKe@FyCZuEj@oEVk@@aAGcBKMBiAKm@EQAOJONCEMG}BcAmDiB_CgAyEgCaDgBeAo@aAm@u@o@mBgBUS}BwAoBmAGm@]@SCeB]oAc@WEuAHaDBgBIuCOO@OBmAr@_@Va@JeAJiCTiD?cEIWBYXe@l@qAxBkCdEkAnB_@v@o@dAw@jAgAxBqBpDs@tAg@dAP^N^wAvBs@fAaCdEcA|AwBbCoBzCsDdHeAvBe@pAuBhG{CvIuAzDGXKxAGjAUdB_@bAuBdG_BrDy@~Aw@fB]d@aAv@uA`AuAv@q@\\{B~@iDjBqDjBiAv@aCrAcB|@g@n@aAUq@GaAIe@CEFE@u@FS@mA\\SHUYy@a@gDMSEuBGS@oDM}Ca@s@Uq@Yw@k@wBkAaAaAs@]q@QeAOg@Gc@??@?@ABADGDCnB@h@RpCaB\\w@Ao@Gs@Yw@e@oCs@Ix@KnH", "distance": 7363.43140614427, "maxNO2": 64, "avgNO2": 42, "maxO3": 23, "avgO3": 21, "maxPM10": 16, "avgPM10": 16, "maxPM25": 6, "avgPM25": 5 }];
-        
+        var detail = e.detail.response;
+
         this.paths = [];
-        
+
         for (var i = 0; i < detail.length; i++) {
             var item = detail[i];
             var encodedPath = item.polyline;
-            
+
             var mapAPI = this.$['map-canvas'].$.api.api;
             var decodedPath = mapAPI.geometry.encoding.decodePath(encodedPath);
-            
+
             // HACK: Item in template repeat does not seem to be able to access functions at bind
             for (var j = 0; j < decodedPath.length; j++) {
                 decodedPath[j].lat = decodedPath[j].lat();
                 decodedPath[j].lng = decodedPath[j].lng();
             }
-            
+
             detail[i].polyline = decodedPath;
         }
-        
-        this.paths = detail;
 
-        // TODO: Handle click events on polylines to display data.
-        // TODO: Switch back to using Heroku Backend app on fixed.
+        this.paths = detail;
     },
     setupInfo: function (e) {
         /* globals google */
@@ -233,11 +226,11 @@ App.Elements['no2pollution-app'] = Polymer({
                 var infoWindow = new google.maps.InfoWindow({
                     content: '<p>Distance: ' + this.paths[index].distance + '</p>'
                 });
-                
+
                 infoWindow.setPosition(e.latLng);
                 infoWindow.open(document.querySelector('#map-canvas').map);
             }
-            
+
             for (var k = 0; k < polylines.length; k++) {
                 google.maps.event.addListener(polylines[k].poly, 'click', createInfoWindow.bind(this, k));
             }
@@ -246,8 +239,7 @@ App.Elements['no2pollution-app'] = Polymer({
     ajaxError: function (e) {
         var detail = e.detail;
         console.log(detail);
-        
-        //this.ajaxResponse(e);
+
         this.fire('toast-message', {
             message: 'Sorry, an error occurred while requesting a route.'
         });
