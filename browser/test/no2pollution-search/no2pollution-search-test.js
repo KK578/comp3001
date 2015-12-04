ab75392fca9301bf544e951c739abfb32c2fad31
Kevin Kwan <KK1241@googlemail.com>
2015-12-04 19:49:52 +0000

    Template element.

---
+++ browser/test/no2pollution-search/no2pollution-search-test.js
@@ -0,0 +1,11 @@
+chai.should();
+
+describe('<no2pollution-search>', function () {
+    var element;
+
+    before(function () {
+        element = document.querySelector('no2pollution-search');
+    });
+
+    it('has no tests written yet');
+});
