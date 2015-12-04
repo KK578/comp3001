e6374177309908f603eabba7b0e1a3665470ece4
Kevin Kwan <KK1241@googlemail.com>
2015-12-04 21:58:31 +0000

    Template no2pollution-route element.

---
+++ browser/test/no2pollution-route/no2pollution-route-test.js
@@ -0,0 +1,11 @@
+chai.should();
+
+describe('<no2pollution-route>', function () {
+	var element;
+
+	before(function () {
+		element = document.querySelector('no2pollution-route');
+	});
+
+    it('has no tests written yet');
+});
