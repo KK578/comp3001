2a2de568f04ea6dde4550268c27388d3a0fb835a
Kevin Kwan <KK1241@googlemail.com>
2015-12-05 21:06:10 +0000

    Hide slider until routes have been requested.

--- browser/custom_components/no2pollution-route/no2pollution-route.js
+++ browser/custom_components/no2pollution-route/no2pollution-route.js
@@ -8,9 +8,7 @@ App.Elements['no2pollution-route'] = Polymer({
      */
     //created: function () {},
     //ready: function () {},
-    attached: function () {
-        this.async(this.ajaxResponse, 1000);
-    },
+    //attached: function () {},
 
     /* https://www.polymer-project.org/1.0/docs/devguide/behaviors.html */
     //behaviors: [],
@@ -44,6 +42,11 @@ App.Elements['no2pollution-route'] = Polymer({
             type: Object
         },
 
+        paths: {
+            type: Array,
+            value: [],
+            observer: 'pathsChanged'
+        },
         infoWindows: {
             type: Array,
             value: []
@@ -52,10 +55,19 @@ App.Elements['no2pollution-route'] = Polymer({
             type: Number,
             value: 0,
             observer: 'toggleRoutesBySlider'
+        },
+        hideSlider: {
+            type: Boolean,
+            value: true
         }
     },
 
     /* Functions specific to this element go under here. */
+    pathsChanged: function (n) {
+        console.log(n);
+        this.$['slider-container'].hidden = n.length === 0;
+    },
+
     sendRequest: function (start, destination) {
         var ajax = this.$.ajax;
 
@@ -67,8 +79,7 @@ App.Elements['no2pollution-route'] = Polymer({
         ajax.generateRequest();
     },
     ajaxResponse: function (e) {
-        //var detail = e.detail.response;
-        var detail = [{ "start": "e179nj", "end": "wc1e6bt", "polyline": "muyyHnpAc@uBl@YtCwApB}@bA[jImCtCcAbAg@lBwAh@a@t@rDZbBVl@T`@pAlAfDtEx@hAbCzD`CtD`ApBLZh@lBBXtAnFTlA^rCVhB`@~BlBtI\\fBZxAnDpN\\bAzAtF|@xC`D~LxC`M~@tClAjETp@H`@L`@P`@x@lCj@dBL`@|BrHpAnEz@bCvDlLl@hBbEjMvDvLtBpFpAzDn@jBjDbLfDhKnAxIHFDR\\vAJFNPFLBHlAt@vABt@IbAWd@OjAo@`@dBh@nC@FAHIJJtAVtFDv@ELC^^HdGlKXKP`AtCbFzF~G@TB\\l@nI\\pGRnBRhARp@Lb@l@rCFr@P~D?XFhEWhAi@|ABFANCBXlAT`BNhBJnBT`C\\rAf@fBvA|Ch@tAZhAXjEfArBbAjBD?BJFN`@n@LVBCDB@HADdCdE~@bBpBfCLHP?AfBCtBDjE@~DIrBKvC`Bj@fAn@z@t@tAnBvAdCdA|AhCzDvCzE|BnExAhE`AnCjAfDfArBfEjInB|DXd@`@j@l@p@xAhB|AtBd@d@~@rAn@~@bAzAl@jAZf@V~@lAtDZhAHb@d@X`@\\nCnCrBhBz@l@lAt@f@RRTPBpANp@Dh@BDhDFbGBrDBfAXrEZdIj@xQVrHF~BPhEDtCTrBZdKB|@H^Fd@Rv@nAfEx@~B|AdF|@lCHd@`@lAx@`CvAxErBlHz@`D`DxM\\`B~AfHHf@Hl@ON?@?@@FBD`@_@ZYPKJJHJ`@c@vCgDHG}@sC", "distance": 12068.522436895895, "maxNO2": 56, "avgNO2": 41, "maxO3": 23, "avgO3": 25, "maxPM10": 15, "avgPM10": 16, "maxPM25": 6, "avgPM25": 6 }, { "start": "e179nj", "end": "wc1e6bt", "polyline": "muyyHnpAc@uBl@YtCwApB}@bA[jImCtCcAbAg@lBwAh@a@t@rDZbBVl@T`@pAlAfDtEx@hAbCzD`CtD`ApBLZh@lBBXtAnFTlA^rCVhB`@~BlBtI\\fBZxAnDpN\\bAzAtF|@xC`D~LxC`M~@tClAjETp@H`@L`@P`@x@lCj@dBL`@|BrHpAnEz@bCvDlLl@hBbEjMvDvLtBpFpAzDn@jBjDbLfDhKnAxIHFDR\\vAJFNPFLBHlAt@vABt@IbAWd@OjAo@`@dBh@nC@FAHIJJtAVtFDv@ELC^^HdGlKXKP`AtCbFzF~G@TB\\l@nI\\pGRnBRhARp@Lb@l@rCFr@P~D?XFhEWhAi@|ABFANCBXlAT`BNhBJnBT`C\\rAf@fBvA|Ch@tAZhAXjEfArBbAjBD?BJFN`@n@LVBCDB@HADdCdE~@bBpBfCLHP?AfBCtBDjE@~DIrBKvC]vEs@lPCTM`DE|@A\\AzAEbCDfDEtEQxD@t@vA|H\\vBxA|JLlBDlAAPDjB@p@GbA@HDVFNFJDBP@GrCDPLNNl@X~BVIV@TDRPNNFNVv@Tp@JTLLNF?LCnBBnBDZJ\\FRb@fDfAzG`@zBb@fDtAhK@?@?@@@D?DADABBJAD@DDD@?hBdLhAfFT`Bx@GvASpBMdD_@xFm@`CUnBMtBTdEd@nATp@Jd@Fl@JLHTZbCfDt@dA`@^V^Zh@dCdDhBjCXZ^^ZRtANbAHRABj@JlDBZVtAjAbEx@|B`AtC~AhFR|@~@vCx@bC^nAvBhHhAlEzB`J`ArEv@bDt@pDNbAOP@F@@@DDGj@i@\\STVzBkCfAgA}@sC", "distance": 12509.169636779592, "maxNO2": 69, "avgNO2": 48, "maxO3": 39, "avgO3": 37, "maxPM10": 13, "avgPM10": 13, "maxPM25": 9, "avgPM25": 6 }, { "start": "e179nj", "end": "wc1e6bt", "polyline": "muyyHnpAd@vBsBbBwCjBw@j@R~FfAdLF\\L\\HbCR~AVfDRpCb@nGz@fNLvBNzBXzGA\\Tz@BpAHbBXzCp@hF`@bCt@~D?Hb@tA{@r@q@r@O|@HPJ\\lCxUz@pGp@~E`AbDx@rCN`A`@vCf@dHJlAh@lFXtBt@zEv@jEn@fCl@fBx@~A~@vBhAhDJRRNfAd@zFvE^\\l@nAj@~@NP~@x@rA|@`Af@Tr@Jb@LHHAKzAYr@Yj@G\\Fb@HbAHzCFrADvBBBCDRb@Zd@Zp@t@bBx@fBtA|Dd@`B`AfCjA~CVVNB`@BPGDBV\\Zy@JWX\\pAvC|A`DhB`EhCfF|@l@d@JpAVRTRVHJDd@DfBh@lCLl@V?tBCTEnAe@x@vDvAlGHZpAbEPt@|ARrAVbBd@jBXIzBKhBGbAQt@EfAA~CIrE?bCLrCNjGHb@tA`F\\`ARbAN|@NtB@f@CBCH?JBFB@?f@?v@Bf@JbANl@RZV`@f@v@d@|@~@pB^f@zAzAv@|Av@lBzCnGn@`Bd@dBVCZlAv@jCz@rDdAfEj@|AfB|DFf@FrJ@`GS?^dHHzCDzG@rGpA?fDl@`@AdAUZbFPvBNClAQ`@BTFVPp@p@\\l@\\|@T~@XhC\\|CN`APf@`A|AjB`C\\j@HQJOPVxB|E\\z@d@dCdBjHb@rBRx@`GhOtAnDH`@@hA?VdE]~Dc@bEa@zASlD]tAMx@GvAS`CQnKiA`CUnBMtBTdEd@nATp@Jz@LVDLHrAhBdAxAt@dA`@^V^Zh@dCdDhBjC^`@XXZRtANbAHRABj@JlDBZLn@Hd@l@vB\\jA^|@t@zB`BlFb@tAR|@~@vCx@bC`C`InAxEvAxFr@rC`ArEv@bDt@pDNbAON?DBD@DDGj@i@\\STVzBkCfAgA}@sC", "distance": 12793.119315919275, "maxNO2": 55, "avgNO2": 41, "maxO3": 33, "avgO3": 35, "maxPM10": 16, "avgPM10": 13, "maxPM25": 8, "avgPM25": 7 }];
+        var detail = e.detail.response;
 
         this.paths = [];
 
@@ -98,7 +109,7 @@ App.Elements['no2pollution-route'] = Polymer({
             function polylineHover(index, e) {
                 var polyline = polylines[index];
                 polyline.strokeColor = '#000';
-                polyline.zIndex = 2;
+                polyline.zIndex = 6;
 
                 var infoWindow = this.infoWindows[index];
                 var position = {
@@ -112,7 +123,7 @@ App.Elements['no2pollution-route'] = Polymer({
             function polylineExit(index) {
                 var polyline = polylines[index];
                 polyline.strokeColor = '#999';
-                polyline.zIndex = 0;
+                polyline.zIndex = 5;
 
                 // Closing immediately causes far too much flashing.
                 // Temporarily just disabling it for now.
